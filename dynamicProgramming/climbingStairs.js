ca060c50e4f73e923bf6f89b1dd123e865289660
Mark-Vale <markpvale@gmail.com>
2021-08-30 12:53:50 -0400

    added new solution to climbingStairs

--- dynamicProgramming/climbingStairs.js
+++ dynamicProgramming/climbingStairs.js
@@ -26,20 +26,36 @@ Constraints:
 
 */
 
+// this code below times out w/ larger inputs:
+// var climbStairs = function(n) {
+//   if (n <= 2) {
+//     return 2
+//   }
+//   if (n === 1) {
+//     return 1
+//   }
+
+//   if (n === 0) {
+//     return 0
+//   }
+
+//   return climbStairs(n - 1) + climbStairs(n - 2)
+
+// }
+
+
 var climbStairs = function(n) {
-  if (n === 2) {
-    return 2
-  }
-  if (n === 1) {
-    return 1
+  const dp = new Array(n).fill(0)
+
+  dp[0] = 0
+  dp[1] = 1
+  dp[2] = 2
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
   }
 
-  if (n === 0) {
-    return 0
-  }
-
-  return climbStairs(n - 1) + climbStairs(n - 2)
-
+  return dp[n]
 }
 
 console.log(climbStairs(5))
